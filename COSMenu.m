40cbf29108f078b50b0d9b889c411999978efb4d
Yecheng Fu <cofyc.jackson@gmail.com>
2010-04-30 21:30:57 +0800

    * fix typo: .gitigore -> .gitignore * learned how to draw arc with center

--- COSMenu.m
+++ COSMenu.m
@@ -47,28 +47,40 @@ static COSMenu *_sharedCOSMenu = nil;
 	 
 - (void)update
 {
+	/**
+	 * @link http://cocoadevcentral.com/d/intro_to_quartz_two/
+	 */
 	#define STRING_ATTR [NSDictionary dictionaryWithObjectsAndKeys: [NSColor whiteColor], NSForegroundColorAttributeName, nil]
 	// Currently, system status bar's height(thickness) and width(length) are 22 pixels.
 	CGFloat imageHeight = 22.0;
 	CGFloat imageWidth = 22.0;
 	CGFloat imageChartRadius = 8.5;
 	NSPoint imageChartCenter = NSMakePoint(imageHeight / 2, imageWidth / 2);
-	NSRect imageChartRect = NSMakeRect(imageChartCenter.x - imageChartRadius, imageChartCenter.y - imageChartRadius, imageChartRadius * 2, imageChartRadius *2);
+	
 	NSImage *myImage = [[NSImage alloc] initWithSize:NSMakeSize(imageHeight,  imageWidth)];
+	
 	_mem_percent += 0.1;
 	NSLog(@"update menu, mem_percent: %f", _mem_percent);
+	
 	[myImage lockFocus];
 	
 	[[NSColor blueColor] set];
-	NSBezierPath* path = [NSBezierPath bezierPathWithOvalInRect: imageChartRect];
+	NSBezierPath* path = [NSBezierPath bezierPath];
+	[path setLineWidth: 1];
+	[path moveToPoint: imageChartCenter];
+	[path appendBezierPathWithArcWithCenter:imageChartCenter radius:imageChartRadius startAngle:90 endAngle:360*_mem_percent clockwise: YES];
+	[[NSColor greenColor] set];
 	[path fill];
-
+	
+	[[NSColor darkGrayColor] set]; 
+	//[path stroke];
+	
 	// draw string example
-	NSAttributedString* stringToDraw = [[NSAttributedString alloc] initWithString:@"%" attributes:STRING_ATTR];
-	NSSize stringSize = [stringToDraw size];
-	stringSize.height -= 1;
-	NSPoint destPoint = NSMakePoint((22 - stringSize.width) / 2, ((22 - stringSize.height) / 2));
-	[stringToDraw drawAtPoint:destPoint];
+//	NSAttributedString* stringToDraw = [[NSAttributedString alloc] initWithString:@"%" attributes:STRING_ATTR];
+//	NSSize stringSize = [stringToDraw size];
+//	stringSize.height -= 1;
+//	NSPoint destPoint = NSMakePoint((22 - stringSize.width) / 2, ((22 - stringSize.height) / 2));
+//	[stringToDraw drawAtPoint:destPoint];
 	
 	[myImage unlockFocus];
 	

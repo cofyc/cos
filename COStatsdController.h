e959efddceeddcff7c43120a729b759f0c7b2820
Yecheng Fu <cofyc.jackson@gmail.com>
2010-05-27 16:52:08 +0800

    repair controller

--- COStatsdController.h
+++ COStatsdController.h
@@ -1,5 +1,5 @@
 #import <Cocoa/Cocoa.h>
-
+#import <Security/Security.h>
 
 @interface COStatsdController : NSObject {
 }
--- COStatsdController.m
+++ COStatsdController.m
@@ -12,10 +12,61 @@ static COStatsdController *_sharedStatsdController= nil;
     return _sharedStatsdController;
 }
 
+- (id)init
+{
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"costatsd" ofType:nil];
+    
+    NSFileManager *fileMgr = [NSFileManager defaultManager];
+    
+    NSDictionary *atts = [fileMgr attributesOfItemAtPath:path error:nil];
+    
+    OSStatus myStatus;
+    
+    if (([atts filePosixPermissions] & 04000) != 04000
+        || [atts fileGroupOwnerAccountID] != [NSNumber numberWithInt:0]
+        || [atts fileGroupOwnerAccountID] != [NSNumber numberWithInt:0]
+        ) {
+        
+        AuthorizationRef myAuthorizationRef;
+        myStatus = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment, kAuthorizationFlagDefaults, &myAuthorizationRef);
+        
+        AuthorizationFlags myFlags = kAuthorizationFlagDefaults
+            | kAuthorizationFlagInteractionAllowed
+            | kAuthorizationFlagPreAuthorize
+            | kAuthorizationFlagExtendRights
+        ;
+        
+        AuthorizationItem myItems = {kAuthorizationRightExecute, 0, NULL, 0};
+        AuthorizationRights myRights = {1, &myItems};
+        myStatus = AuthorizationCopyRights (myAuthorizationRef,&myRights, kAuthorizationEmptyEnvironment, myFlags, NULL );
+        if (myStatus |= errAuthorizationSuccess) {
+            
+        }
+        
+        FILE *myCommunicationPipe = NULL;
+        char *myArguments[] = {
+            "repair",
+        };
+        
+        myStatus = AuthorizationExecuteWithPrivileges(myAuthorizationRef, [path UTF8String], kAuthorizationFlagDefaults, myArguments, &myCommunicationPipe);
+        
+        if (myStatus == errAuthorizationSuccess) {
+            NSFileHandle *fileHandle = [[NSFileHandle alloc] initWithFileDescriptor: fileno(myCommunicationPipe)];
+            NSLog(@"costatsd:%@", [fileHandle readDataToEndOfFile]);
+        }
+        
+        AuthorizationFree(myAuthorizationRef, kAuthorizationFlagDefaults);
+    }
+    
+    return self;
+}
+
 - (CGFloat)getPercent
 {
     
     return 0.4;
 }
+
+
 
 @end

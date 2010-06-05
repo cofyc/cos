9fa85d0a4edb581637ba378c972a78ee5cf67f81
Yecheng Fu <cofyc.jackson@gmail.com>
2010-06-05 17:06:38 +0800

    implement -keyDown: method

--- COPrefsWindowController.m
+++ COPrefsWindowController.m
@@ -25,7 +25,18 @@
 {
     [self willChangeValueForKey:@"startAtLogin"];
     [COLoginItem setStartAtLogin:[self appURL] enabled:enabled];
-    [self didChangeValueForKey:@"startAtLogin"]; 
+    [self didChangeValueForKey:@"startAtLogin"];
+}
+
+/* NSWindow itself is actually a fairly lightweight class, and most of what we think of as standard window behavior is done by the frame view.
+ so we implement the -keyDown: method that receives those keystrokes and handle them */
+- (void)keyDown:(NSEvent *)theEvent
+{
+    if ([theEvent modifierFlags] & NSCommandKeyMask) {
+        if ([[theEvent characters] isEqualToString:@"w"]) {
+            [self close];
+        }
+    }
 }
 
 @end
